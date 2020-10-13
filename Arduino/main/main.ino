b65a388dd7879171f8b7d8b370feaad33749d020
Andreas Michaelides <michaelidesandreas1801@gmail.com>
2020-10-13 13:09:11 +0200

    Update main.ino

--- Arduino/main/main.ino
+++ Arduino/main/main.ino
@@ -119,89 +119,13 @@ void loop()
  // Buzzer
  // -------------------
  
- fall = FallDetected();
+  fall = FallDetected();
+ 
   if (emergencyStatus == true){
     digitalWrite(6, HIGH);
   }
 
- mpu_read();
- 
- //2050, 77, 1947 are values for calibration of accelerometer
- // values may be different for you
- ax = (AcX-2050)/16384.00;
- ay = (AcY-77)/16384.00;
- az = (AcZ-1947)/16384.00;
-
- //270, 351, 136 for gyroscope
- gx = (GyX+270)/131.07;
- gy = (GyY-351)/131.07;
- gz = (GyZ+136)/131.07;
-
- // calculating Amplitute vactor for 3 axis
- float Raw_AM = pow(pow(ax,2)+pow(ay,2)+pow(az,2),0.5);
- int AM = Raw_AM * 10;  // as values are within 0 to 1, I multiplied 
-                        // it by for using if else conditions 
-
- Serial.println(AM);
- //Serial.println(PM);
- //delay(500);
-
- if (trigger3==true){
-    trigger3count++;
-    //Serial.println(trigger3count);
-    if (trigger3count>=10){ 
-       angleChange = pow(pow(gx,2)+pow(gy,2)+pow(gz,2),0.5);
-       //delay(10);
-       Serial.println(angleChange); 
-       if ((angleChange>=0) && (angleChange<=10)){ //if orientation changes remains between 0-10 degrees
-           fall=true; trigger3=false; trigger3count=0;
-           Serial.println(angleChange);
-             }
-       else{ //user regained normal orientation
-          trigger3=false; trigger3count=0;
-          Serial.println(F("TRIGGER 3 DEACTIVATED"));
-       }
-     }
-  }
-
- if (trigger2count>=6){ //allow 0.5s for orientation change
-   trigger2=false; trigger2count=0;
-   Serial.println("TRIGGER 2 DECACTIVATED");
-   }
- if (trigger1count>=6){ //allow 0.5s for AM to break upper threshold
-   trigger1=false; trigger1count=0;
-   Serial.println(F("TRIGGER 1 DECACTIVATED"));
-   }
- if (trigger2==true){
-   trigger2count++;
-   //angleChange=acos(((double)x*(double)bx+(double)y*(double)by+(double)z*(double)bz)/(double)AM/(double)BM);
-   angleChange = pow(pow(gx,2)+pow(gy,2)+pow(gz,2),0.5); Serial.println(angleChange);
-   if (angleChange>=30 && angleChange<=400){ //if orientation changes by between 80-100 degrees
-     trigger3=true; trigger2=false; trigger2count=0;
-     Serial.println(angleChange);
-     Serial.println(F("TRIGGER 3 ACTIVATED"));
-       }
-   }
- if (trigger1==true){
-   trigger1count++;
-   if (AM>=12){ //if AM breaks upper threshold (3g)
-     trigger2=true;
-     Serial.println(F("TRIGGER 2 ACTIVATED"));
-     trigger1=false; trigger1count=0;
-     }
-   }
- if (AM<=2 && trigger2==false){ //if AM breaks lower threshold (0.4g)
-   trigger1=true;
-   Serial.println(F("TRIGGER 1 ACTIVATED"));
-   }
- //Delay is needed to prevent clogging the port
   delay(100);
-
-
- 
-
-
-
 }
 
 // ===================
@@ -233,14 +157,6 @@ void wrongPulse(){
   }
   
   emergencyStatus = true;  
-}
-
-// Called when fall has been detected
-void fallOccurance(){
-   Serial.println(F("FALL DETECTED"));
-   emergencyStatus = true;
-   sendMessage(F("Fall Detected!"), F("I have fallen, if no reset message appears within a minute, please send help!")); 
-   fall = false; 
 }
 
 // Called when reset button is pressed
@@ -368,7 +284,6 @@ bool FallDetected(){
 
   return fall; 
 }
-
 
 // Email Sending function
 void sendMessage(String sbj, String msg){
